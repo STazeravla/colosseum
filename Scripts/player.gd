be0a2af2f05107b68e9d14c40ff84047cd7d9b86
santi <santinoalvarez325@gmail.com>
2026-03-29 20:34:11 -0300

    add player movement system

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,17 +1,38 @@
 extends CharacterBody2D
 
-@export var speed := 80.0 # más lento que normal
+@onready var animatedsprite = $AnimatedSprite2D
+var speed = 100.0
+var last_direction = "down"
 
-func _physics_process(delta):
-	var direction = 0
-	
-	if Input.is_action_pressed("ui_right"):
-		direction += 1
-	if Input.is_action_pressed("ui_left"):
-		direction -= 1
-	
-	velocity.x = direction * speed
-	
+func _physics_process(delta: float) -> void:
+	get_input()
 	move_and_slide()
+
+func get_input():
+	var input_direction = Input.get_vector("Left", "Right", "Up", "Down")
+	print(input_direction)
 	
+	if input_direction == Vector2.ZERO:
+		velocity = Vector2.ZERO
+		update_animation("idle")
+		return
+
+	if abs(input_direction.x) > abs(input_direction.y):
+		#Movimiento horizontal
+		if input_direction.x > 0:
+			last_direction = "right"
+		else:
+			last_direction = "left"
+	else:
+		#Movimiento vertical
+		if input_direction.y > 0:
+			last_direction = "down"
+		else:
+			last_direction = "up"
+		
+	update_animation("run")
+	velocity = input_direction * speed
+
+func update_animation(state):
+	animatedsprite.play(state + last_direction)
 	
